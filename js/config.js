41e3af27158d1867edc74afa6f01598578d26e06
Washingtonbarbalho <137066669+Washingtonbarbalho@users.noreply.github.com>
2025-10-10 00:13:43 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// Arquivo de Configuração
+// Armazena constantes e configurações usadas em todo o sistema.
+
+export const ADMIN_EMAIL = "washington.wn8@gmail.com";
+
+// Fatores de parcelamento
+export const FATORES = {
+    cartao: [1.0292, 0.5220, 0.3530, 0.2685, 0.2179, 0.1841, 0.1600, 0.1420, 0.1280, 0.1168, 0.1076, 0.1000],
+    carne: [1.0690, 0.5523, 0.3804, 0.2946, 0.2432, 0.2091, 0.1849, 0.1668, 0.1528, 0.1417, 0.1327, 0.1252]
+};
